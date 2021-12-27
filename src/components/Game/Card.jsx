7f6d460c7ddf558fb1b30b2a372e54a124cf20ae
galatanw <buisness.galatanw@gmail.com>
2021-12-27 09:16:00 +0200

    backgrounds and card images

--- src/components/Game/Card.jsx
+++ src/components/Game/Card.jsx
@@ -4,9 +4,7 @@ export default function Card (props) {
             <div id={props.index} onClick={props.tryCard} className={props.card.revealed === false ?props.style2.notFound :props.style2.found}>
             </div>
             <button className={props.buttonStyle} disabled={props.card.found}  >
-                <div>
-                    <h1 >{props.card.value}</h1>
-                </div>
+                  <img src={props.card.value} alt="" /> <span> </span>
             </button>
         </div>
         )
--- src/components/Game/FullGame.jsx
+++ src/components/Game/FullGame.jsx
@@ -3,12 +3,27 @@ import MemoryGame from './MemoryGame'
 import * as styles from './FullGame.module.css'
 export default class FullGame extends Component {
     state={startGame:false,array:[]}
- 
+     PicsArray=[
+     " https://i.ibb.co/8rvbyDp/Goku-Limit-Breaker-Torneo-del-Poder-DRAG-N.jpg",
+"https://i.ibb.co/MfwfpWJ/Dragon-Ball-Z-Red-Goku-IPhone-Wallpaper-IPhone-Wallpapers.jpg",
+"https://i.ibb.co/y6TmrHh/Dragon-Ball-Z-Super-Dbz-Poster-by-Breanna-Gulgowski-Displate.jpg",
+"https://i.ibb.co/vQhxwG3/DBZ-alternative-future-Gohan-ssj-2.jpg",
+"https://i.ibb.co/SX986by/Goku-and-Shenron.png",
+"https://i.ibb.co/vLsd3CS/Shenron-Shenlong-The-Holy-Dragon-by-Zackary-on-Deviant-Art.jpg",
+"https://i.ibb.co/gZC15NG/Daizenshuu8-16.jpg",
+"https://i.ibb.co/7zy7XdH/DBS-saga-de-Mirai-Trunks-o-de-Black-Como.jpg",
+"https://i.ibb.co/NLGrB9H/6cdcf4f6-6889-4fb7-86fa-65d3a41b977a.jpg",
+"https://i.ibb.co/HFHjxSP/Lord-Beerus-by-Nova-Sayajin-Goku-on-Deviant-Art.jpg",
+"https://i.ibb.co/MMVsq9x/Goku.jpg",
+"https://i.ibb.co/hCmqXv3/0581ad02-6f9e-48d3-add4-9209a95b838d.png",
+"https://i.ibb.co/XDLs2df/61f422d4-d35d-41ab-a766-8245a4ea07a0.jpg",
+"https://i.ibb.co/ngpwyTh/e6ebc012-4866-40c2-8fe3-47290909faf6.jpg"
+    ]
     startMemoryGame=(e)=>{  
      e.preventDefault()
       const temp=[]
       for (let index = 0; index < Number(e.target.select.value); index++) {
-      temp.push(index+1)
+      temp.push(this.PicsArray[index])
     }
     this.setState({startGame:true,array:temp})
     }
@@ -29,9 +44,8 @@ export default class FullGame extends Component {
             <input type="submit" value={"start"} />
           </form>
           }</div>
-          <div className={styles.game}>
-        {this.state.startGame?<MemoryGame cards={this.state.array}/>:null}
-        </div>
+          
+          {this.state.startGame?<div className={styles.game}><MemoryGame cards={this.state.array}/></div>:null}
         </div> 
       );
     }
